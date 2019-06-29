c2c37482a9d959a2c8de19454e913f4b0f352311
shmuelgarber1@gmail.com <shmuelgarber1@gmail.com>
2019-06-29 23:14:15 +0300

    hearot1

--- src/elements/Light.java
+++ src/elements/Light.java
@@ -2,15 +2,20 @@ package elements;
 import primitives.Point3D;
 import java.awt.Color;
 public abstract class Light {
+    Color color;
+
+    /********** Constructors ***********/
+    public Light(Color color) {
+        this.color = new Color(color.getRGB());
+    }  //copy constructor
+
+
+    /************** Getters/Setters *******/
     public Color getColor() {
         return color;
-    }
+    }             //get the color
 
-    Color color;
-    public abstract Color getIntensity(Point3D tmp);
 
-    public Light(Color color) {
-        this.color = color;
-    }
-
+    /************** Operations ***************/
+    public abstract Color getIntensity(Point3D tmp);      //abstract function that all lights have to implement
 }
