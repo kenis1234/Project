06882f9bd3977c45d73ac1e3cab261c7bab51dce
meirbenchimol <benchimolmeir@gmail.com>
2019-07-03 00:29:23 +0300

    pointLitght Test

--- src/unitTests/ColorTests.java
+++ src/unitTests/ColorTests.java
@@ -1,9 +1,8 @@
+// Bs"d
+
 package unitTests;
 
-import elements.AmbientLight;
-import elements.Camera;
-import elements.LightSource;
-import elements.SpotLight;
+import elements.*;
 import geometries.*;
 import geometries.Triangle;
 import org.junit.Test;
@@ -111,6 +110,38 @@ public class ColorTests {
 
 
         ImageWriter imageWriter = new ImageWriter("Spot test with shadow2", 500, 500, 500, 500);
+
+        Render render = new Render(imageWriter, scene);
+        render.renderImage();
+    }
+
+    @Test
+    public void pointLightTest2() {
+        Scene scene = new Scene("");
+        scene.setBackground(new Color(0, 0, 0));
+        scene.setAmbient(new AmbientLight(new Color(255, 255, 255), 0.1));
+        scene.setCameraAndDistance(new Camera(), 200);
+
+        Sphere sphere = new Sphere(500, new Point3D(0.0, 0.0, -1000));
+        sphere.setMaterial(new Material(1, 1, 1, 1, 20));
+        sphere.setEmission(new Color(0, 0, 100));
+        scene.addGeometry(sphere);
+
+        Triangle triangle = new Triangle(
+                new Point3D(-125, -225, -260),
+                new Point3D(-225, -125, -260),
+                new Point3D(-225, -225, -270));
+
+        triangle.setEmission(new Color(0, 0, 100));
+        triangle.setMaterial(new Material(0.3, 2, 0, 0, 4));
+        scene.addGeometry(triangle);
+
+        scene.addLight(new PointLight(
+                new Color(255, 100, 100), new Point3D(-200, -200, -150),
+                0.1, 0.00001, 0.000005));
+
+
+        ImageWriter imageWriter = new ImageWriter("Point test with shadow", 500, 500, 500, 500);
 
         Render render = new Render(imageWriter, scene);
         render.renderImage();
